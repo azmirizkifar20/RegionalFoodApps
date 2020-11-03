be7bd8ce911d6f623719e597e868a332edd24523
azmirf20 <m.azmirizkifar20@gmail.com>
2020-11-03 18:09:04 +0700

    code cleanup & remove unused resources

--- app/src/main/java/org/marproject/makanankhasindonesia/ui/profile/ProfileFragment.kt
+++ app/src/main/java/org/marproject/makanankhasindonesia/ui/profile/ProfileFragment.kt
@@ -1,7 +1,5 @@
 package org.marproject.makanankhasindonesia.ui.profile
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- core/src/main/java/org/marproject/makanankhasindonesia/core/utils/AppExecutors.kt
+++ core/src/main/java/org/marproject/makanankhasindonesia/core/utils/AppExecutors.kt
@@ -1,7 +1,5 @@
 package org.marproject.makanankhasindonesia.core.utils
 
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
@@ -16,11 +14,4 @@ class AppExecutors @VisibleForTesting constructor(
 
     fun diskIO(): Executor = diskIO
 
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
 }
