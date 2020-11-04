db2c072e4e34c81624dc4fd3dcd93ecf1c42e65a
azmirf20 <m.azmirizkifar20@gmail.com>
2020-11-04 08:43:00 +0700

    Implement security

--- core/src/main/java/org/marproject/makanankhasindonesia/core/di/CoreModule.kt
+++ core/src/main/java/org/marproject/makanankhasindonesia/core/di/CoreModule.kt
@@ -3,6 +3,7 @@ package org.marproject.makanankhasindonesia.core.di
 import androidx.room.Room
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -35,10 +36,18 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "firebasestorage.googleapis.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/ikLJDXn+JjUmyrM8hrQniBSqiWCnkbY7xXANNf/BbAQ=")
+            .add(hostname, "sha256/5j6bIxVylmLBIeyADL48ObCTTKoClAUnc1Tp5nRLsqw=")
+            .add(hostname, "sha256/7rlXmYCbZOgh8zbA0JD9hsUELldvDrUyH5gHQiSzJsU=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
