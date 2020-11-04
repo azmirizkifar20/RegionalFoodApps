fae5ba1bc22864b26bd0a0edd43de6127fb094a9
azmirf20 <m.azmirizkifar20@gmail.com>
2020-11-04 13:55:25 +0700

    remove deprecated code

--- app/src/main/java/org/marproject/makanankhasindonesia/ui/home/HomeFragment.kt
+++ app/src/main/java/org/marproject/makanankhasindonesia/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package org.marproject.makanankhasindonesia.ui.home
 
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.net.Uri
@@ -8,6 +9,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -112,20 +114,15 @@ class HomeFragment : Fragment() {
     private val offsetListener =
         AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
             var scrollRange = -1
+            val wrappedDrawable = DrawableCompat.wrap(drawable)
+
             if (scrollRange == -1)
                 scrollRange = appBarLayout.totalScrollRange
 
-            if (scrollRange + verticalOffset == 0) {
-                drawable.setColorFilter(
-                    ContextCompat.getColor(requireContext(), android.R.color.white),
-                    PorterDuff.Mode.SRC_ATOP
-                )
-            } else {
-                drawable.setColorFilter(
-                    ContextCompat.getColor(requireContext(), R.color.color_primary),
-                    PorterDuff.Mode.SRC_ATOP)
-
-            }
+            if (scrollRange + verticalOffset == 0)
+                DrawableCompat.setTint(wrappedDrawable, ContextCompat.getColor(requireContext(), android.R.color.white))
+             else
+                DrawableCompat.setTint(wrappedDrawable, ContextCompat.getColor(requireContext(), R.color.color_primary))
         }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
